ca63be14fd963f3873a0a66ab3e6bca781825674
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-07-27 16:41:04 -0700

    added form for gem and killed my shoe controller code

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,33 +1,31 @@
 class ShoesController < ApplicationController
+  # before_action :authenticate, :authorize
+
   def index
     @shoes = Shoe.all
   end
-
   def new
     @shoe = Shoe.new
-    @user = current_user
   end
 
   def create
-    @user = User.find(params[:id])
-    @shoe = Shoe.new(shoe_params)
-    @user.shoes << @shoe
-    if @shoe.save
+    shoe = Shoe.new(shoe_params)
+    shoe[:user_id] = user.id
+    @user.shoes << shoe
+    if shoe.save
       # current_user.cars << @car
-      redirect_to "users/user_id/shoes/#{@shoe.id}"
+      redirect_to user_path(@user), notice: "New Shoe Added!"
     else
       render 'new'
     end
   end
 
   def show
-    @shoe = Shoe.find_by_id(params[:id])
-    @user = User.find_by_id(params[:id])
+    @shoe = Shoe.find(params[:id])
   end
 
   def edit
     @shoe = Shoe.find(params[:id])
-    @user = Shoe.find(params[:user_id])
   end
 
   def update
@@ -40,16 +38,36 @@ class ShoesController < ApplicationController
   end
 
   def destroy
-     @shoe = Shoe.find(params[:id])
-     if @shoe.delete
+     shoe = Shoe.find(params[:id])
+     if shoe.destroy
+      redirect_to root_path, notice: "Shoe Removed!"
+     else
+      flash.now.alert ="Error trying to delete Shoe."
       redirect_to root_path
     end
  end
 
-  private
+private
+
+  # def authenticate
+  #   unless logged_in?
+  #     flash.now.alert =
+  #       "You must be logged in to access this section of the site."
+  #     redirect_to login_path
+  #   end
+  # end
+
+  # def authorize
+  #   @user = User.find(params[:user_id])
+
+  #   unless current_user.user == @user
+  #     flash.now.alert =
+  #       "You are not authorized to edit this Shoe's information."
+  #     redirect_to login_path
+  #   end
+  # end
 
   def shoe_params
-    params.require(:shoe).permit(:name, :condition, :asking_price, :image)
+    params.require(:shoe).permit(:name, :condition, :asking_price, :image, :user_id)
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+skip_before_action :require_login, only: [:new, :create]
+
  def index
     @users = User.all
   end
@@ -8,19 +10,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:id])
-    @shoe = Shoe.find_by_id(params[:user_id])
+    @user = User.find(params[:id])
 
   end
 
   def edit
-    @user = User.find_by_id(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to "/users/#{@user.id}"
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
@@ -37,9 +38,26 @@ class UsersController < ApplicationController
   end
 
 private
-def user_params
-  params.require(:user).permit(:name, :email, :address_line_1, :address_line_2, :city, :zip_code, :state, :password, :password_confirmation, :image)
-end
+  def authenticate
+    unless logged_in?
+      flash.now.alert =
+        "You must be logged in to access this section of the site."
+      redirect_to login_path
+    end
+  end
 
+  def authorize
+    @user = User.find(params[:user_id])
+
+    unless current_user.user == @user
+      flash.now.alert =
+        "You are not authorized to edit this Shoe's information."
+      redirect_to user_path(@user)
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :address_line_1, :address_line_2, :city, :zip_code, :state, :password, :password_confirmation, :image)
+  end
 
 end
