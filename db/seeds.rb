818cb46d1ffc4222fcbbd84f3af22dce7e704f81
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-07-30 09:27:39 -0700

    updated views with shoe size

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,12 @@ User.create(
   id: 1,
   name: "Jaytee",
   email: "jaytee.sanchez@gmail.com",
+  address_line_1: "10000 Imperial Hwy Apt E207",
+  address_line_2:,
+  city: "Downey",
+  zip_code: 90242,
+  state: "CA",
   password: "a",
   password_confirmation: "a"
+  image: "https://avatars1.githubusercontent.com/u/12928206?v=3&s=460",
   )
